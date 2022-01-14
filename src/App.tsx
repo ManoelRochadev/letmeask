fba10cad55b8a5d49661da2da2e149eb923e3ec1
Manoel Rocha <manoelmelo367@gmail.com>
2022-01-14 00:18:03 +0000

    autenticação completa

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { Button } from './components/button'
+import { auth, firebase } from './services/firebase';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { NewRoom } from "./pages/NewRoom";
+import { AuthContextProvider } from './contexts/AuthContext';
 
 function App() {
   return (
-    <div>
-      <h1>Hello word</h1>
-      <Button />
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path='/' exact component={Home} />
+        <Route path='/rooms/new' component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
